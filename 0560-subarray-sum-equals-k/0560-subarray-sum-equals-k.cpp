8ea300f36c1241317a4f2c655505f22f8e76d596
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-07-12 10:46:13 +0530

    Time: 66 ms (68.88%), Space: 45.4 MB (48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // unordered_map<int, int> mpp;
+        // mpp[0] = 1;
+        // int preSum=0;
+        // int count=0;
+        
+        // for(int i = 0; i < nums.size(); i++){
+        //     preSum = preSum + nums[i];
+
+        //     int remove = preSum - k;
+        //     count = count + mpp[remove];
+        //     mpp[preSum]++;
+        // }
+
+        // return count;
+        int n = nums.size();
         unordered_map<int, int> mpp;
         mpp[0] = 1;
-        int preSum=0;
-        int count=0;
-        
-        for(int i = 0; i < nums.size(); i++){
-            preSum = preSum + nums[i];
+        int presum = 0;
+        int count = 0;
 
-            int remove = preSum - k;
-            count = count + mpp[remove];
-            mpp[preSum]++;
+        for(int i = 0; i < n; i++) {
+            presum += nums[i];
+            int remove = presum - k;
+            if(mpp.find(remove) != mpp.end()) {
+                count += mpp[remove];
+            }
+            mpp[presum]++;
         }
-        
+
         return count;
     }
 };
