d66e5894ed8af0120b60cc56549616630f3caca0
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-07-30 12:07:51 +0530

    Time: 7 ms (96.44%), Space: 20.6 MB (46.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int expected_sum = (n * (n + 1))/2;     // Sum of numbers from 0 to n
+        // int n = nums.size();
+        // int expected_sum = (n * (n + 1))/2;     // Sum of numbers from 0 to n
 
-        int actual_sum = 0;
-        for(auto num:nums){
-            actual_sum += num;
+        // int actual_sum = 0;
+        // for(auto num:nums){
+        //     actual_sum += num;
+        // }
+        // return (expected_sum - actual_sum);
+
+
+        int n = nums.size();
+
+        int xor1 = 0;
+        int xor2 = 0;
+
+        // iterating through the array and numbers from 0 to n..
+        for(int i = 0; i < n; i++){
+            xor1 = xor1 ^ i;
+            xor2 = xor2 ^ nums[i];
         }
-        return (expected_sum - actual_sum);
+
+        // include the last number n in xor1 calc
+        xor1 = xor1 ^ n;
+
+        return xor1 ^ xor2;
     }
 };
