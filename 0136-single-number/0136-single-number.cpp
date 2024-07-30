8a9d628f8c9e534f70d7b6686096096aa8783591
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-07-30 12:30:40 +0530

    Time: 15 ms (52.04%), Space: 19.5 MB (37.18%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
 
-    // time and space complexity is O(n) and O(n)
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mpp;
+    // // time and space complexity is O(n) and O(n)
+    // int singleNumber(vector<int>& nums) {
+    //     unordered_map<int, int> mpp;
 
-        // Count the occurrences of each number
-        for(auto num:nums){
-            mpp[num]++;
+    //     // Count the occurrences of each number
+    //     for(auto num:nums){
+    //         mpp[num]++;
+    //     }
+
+    //     for(auto it : mpp){
+    //         if(it.second == 1){
+    //             return it.first;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+    
+
+
+    // time and space complexity is O(n) and O(1)
+    int singleNumber(vector<int>& nums){
+        int result = 0;
+        for(auto num : nums){
+            result = result ^ num;
         }
-
-        for(auto it : mpp){
-            if(it.second == 1){
-                return it.first;
-            }
-        }
-
-        return -1;
+        
+        return result;
     }
 };
