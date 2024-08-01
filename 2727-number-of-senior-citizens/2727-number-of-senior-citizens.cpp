2a7c64aa3d9e6da71064f9c02b552604afca853a
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-01 10:20:29 +0530

    Time: 3 ms (91.87%), Space: 17.6 MB (27.35%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int n = details.size();
-        string age_str;
-        int count = 0;
 
-        for(auto detail : details){
-            age_str = detail.substr(11, 2);       //extract age
-            int age = stoi(age_str);
+        int count = 0;
+        for(const auto& detail : details){
+            // string age_str = detail.substr(11, 2);       //extract age
+            // int age = stoi(age_str);
+            
+            int age = (detail[11] - '0' ) * 10 + (detail[12] - '0');
 
             if(age > 60){
                 count++;
