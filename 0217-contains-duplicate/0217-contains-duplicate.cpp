c2efcf5eb9bfab2235d36537ed9191cb7d561f25
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-08 16:08:02 +0530

    Time: 112 ms (23.35%), Space: 75.5 MB (13.36%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for(const auto& num : nums){
+            mpp[num]++;
+        }
+
+        for(auto it : mpp){
+            if(it.second > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
