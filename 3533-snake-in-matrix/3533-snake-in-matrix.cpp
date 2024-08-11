b481c3ee75758a5a983cec43f28e269d977dbeac
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-11 11:20:08 +0530

    Time: 8 ms (100%), Space: 35.2 MB (60%) - LeetHub

--- 3533-snake-in-matrix/3533-snake-in-matrix.cpp
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -6,39 +6,43 @@ public:
 
         for(const auto& command : commands){
             if(command == "UP"){
-                if(x == 0){
-                    x = 0;
-                }
-                else {
-                    x = x - 1;
-                }
+                // if(x == 0){
+                //     x = 0;
+                // }
+                // else {
+                //     x = x - 1;
+                // }
+                x = x - 1;
             }
 
             else if(command == "DOWN"){
-                if(x == n - 1){
-                    x = n - 1;
-                }
-                else{
-                    x = x + 1;
-                }
+                // if(x == n - 1){
+                //     x = n - 1;
+                // }
+                // else{
+                //     x = x + 1;
+                // }
+                x = x + 1;
             }
 
             else if(command == "RIGHT"){
-                if(y == n - 1){
-                    y = n - 1;
-                }
-                else{
-                    y = y + 1;
-                }
+                // if(y == n - 1){
+                //     y = n - 1;
+                // }
+                // else{
+                //     y = y + 1;
+                // }
+                y = y + 1;
             }
 
             else if(command == "LEFT"){
-                if(y == 0){
-                    y = 0;
-                }
-                else{
-                    y = y - 1;
-                }
+                // if(y == 0){
+                //     y = 0;
+                // }
+                // else{
+                //     y = y - 1;
+                // }
+                y = y - 1;
             }
         }
 
