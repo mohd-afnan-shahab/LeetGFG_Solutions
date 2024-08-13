1ad01240cda4affbe1ae0bf74c306c8423fff6b1
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-13 15:22:49 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.06%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == val) {
-                // Shift elements to the left
-                for (int j = i; j < n - 1; j++) {
-                    nums[j] = nums[j + 1];
-                }
-                n--; // Reduce size of array
-                i--; // Decrement index to check the current position again
+
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
             }
         }
-        return n;
+        return count;
     }
 };
