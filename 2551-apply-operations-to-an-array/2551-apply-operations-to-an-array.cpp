16342d4a265d1e555d3ff0aa232317629a0f1432
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-14 18:22:44 +0530

    Time: 7 ms (15.64%), Space: 11.5 MB (18.95%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -18,7 +18,11 @@ public:
             }
         }
 
-        fill(nums.begin() + j, nums.end(), 0);
+        while(j < n){
+            nums[j] = 0;
+            j++;
+        }
+        // fill(nums.begin() + j, nums.end(), 0);
 
         return nums;
     }
