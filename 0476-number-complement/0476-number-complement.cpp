6e8864ca0cfbf13bb94db8efb47fb63351929b2d
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-22 13:45:10 +0530

    Time: 3 ms (41.07%), Space: 7.5 MB (57.13%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int findComplement(int num) {
         
-        // Find the bit length of a num
+        // Find the bit length of num
         int bitlength = (int)(log2(num)) + 1; 
 
+        // Create a mask with all bits set to 1 for the length of num
         unsigned int mask = (1U << bitlength) - 1;
 
+        // XOR num with mask to flip all bits
         return num ^ mask;
     } 
 };
