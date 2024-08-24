a3e1c69b3ac89e9816471359b94ce82b14a82a04
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-24 23:51:32 +0530

    Time: 0 ms (100%), Space: 10.8 MB (67.97%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,16 +1,36 @@
+// class Solution {
+// public:
+//     vector<string> fizzBuzz(int n) {
+//         vector<string> ans;
+
+//         for(int i = 1; i <= n; i++){
+//             if(i % 3 == 0 && i % 5 == 0) ans.push_back("FizzBuzz");
+//             else if(i % 5 == 0) ans.push_back("Buzz");
+//             else if(i % 3 == 0) ans.push_back("Fizz");
+
+//             // If none of the above conditions are true, add the number itself as a string
+//             else ans.push_back(to_string(i));
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> ans;
+        vector<string> result(n);
 
         for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 && i % 5 == 0) ans.push_back("FizzBuzz");
-            else if(i % 5 == 0) ans.push_back("Buzz");
-            else if(i % 3 == 0) ans.push_back("Fizz");
+            string temp = "";
+            if(i % 3 == 0) temp += "Fizz";
+            if(i % 5 == 0) temp += "Buzz";
 
-            // If none of the above conditions are true, add the number itself as a string
-            else ans.push_back(to_string(i));
+            // If neither "Fizz" nor "Buzz" was added, convert the number to a string
+            if(temp.empty()) temp = to_string(i);
+
+            result[i - 1] = temp;
         }
-        return ans;
+        return result;
     }
 };
