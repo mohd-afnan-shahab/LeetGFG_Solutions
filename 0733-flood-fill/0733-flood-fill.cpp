abcd8317eacc3f0cebf05a9ee2923711c1d1de4e
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-26 22:49:43 +0530

    Time: 12 ms (12.47%), Space: 16.7 MB (90.6%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     
-    void DFS(int row, int col, vector<vector<int>>& image, vector<vector<int>>& ans,
+    void DFS(int row, int col, vector<vector<int>>& image, vector<vector<int>>& filledImage,
              int deltaRow[], int deltaCol[], int initialColor, int color){
+        
+        // Update the current cell with the new color
+        filledImage[row][col] = color;
 
-        ans[row][col] = color;
-
-        int m = ans.size();
-        int n = ans[0].size();
+        int m = filledImage.size();
+        int n = filledImage[0].size();
 
         for(int i = 0; i < 4; i++){
             int nRow = row + deltaRow[i];
             int nCol = col + deltaCol[i];
 
             if(nRow >= 0 && nRow < m && nCol < n && nCol >= 0 &&
-               image[nRow][nCol] == initialColor && ans[nRow][nCol] != color
+               image[nRow][nCol] == initialColor && filledImage[nRow][nCol] != color
             ){
-                DFS(nRow, nCol, image, ans, deltaRow, deltaCol, initialColor, color);
+                DFS(nRow, nCol, image, filledImage, deltaRow, deltaCol, initialColor, color);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int initialColor = image[sr][sc];   
-        vector<vector<int>> ans = image;
 
-        // all 4 directions
+        vector<vector<int>> filledImage = image;    // Copy of the original image to modify
+
+        // Possible moves: up, right, down, left
         int deltaRow[] = {-1, 0, 1, 0};
         int deltaCol[] = {0, 1, 0, -1};
 
-        DFS(sr, sc, image, ans, deltaRow, deltaCol, initialColor, color);
+        // Start the flood fill process from the starting pixel
+        DFS(sr, sc, image, filledImage, deltaRow, deltaCol, initialColor, color);
 
-        return ans;
+        return filledImage;
     }
 };
