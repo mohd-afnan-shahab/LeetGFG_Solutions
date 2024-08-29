392d7f3826e223d442ae74ae0797ebed4f96b148
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-08-29 17:24:15 +0530

    Time: 17 ms (63.52%), Space: 16.3 MB (68.7%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    bool check(int start, vector<vector<int>>& graph, vector<int>& color){
+    bool DFS(int node, int currcolor, vector<int>& color, vector<vector<int>>& graph){
+        color[node] = currcolor;
 
-        int V = graph.size();
-        queue<int> q;
-        q.push(start);
-
-        // initially give color 1 to node 0
-        color[start] = 1;
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            for(const auto& adjNode : graph[node]){
-                // if adj Node is not coloured
-                if(color[adjNode] == -1){
-                    color[adjNode] = !color[node];      // Assign the opp color
-                    q.push(adjNode);
-                }
-
-                // if adjNode color comes out to be same of node color
-                if(color[adjNode] == color[node]){
+        for(auto adjNode : graph[node]){
+            if(color[adjNode] == -1){
+                if(DFS(adjNode, !currcolor, color, graph) == false){
                     return false;
                 }
+            }
+            if(color[adjNode] == currcolor){
+                return false;
             }
         }
         return true;
     }
-
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
-
-        // track the color of every node (-1 means no color)
         vector<int> color(V, -1);
 
         for(int i = 0; i < V; i++){
             if(color[i] == -1){
-                if(check(i, graph, color) == false){
+                if(DFS(i, 0, color, graph) == false){
                     return false;
                 }
             }
