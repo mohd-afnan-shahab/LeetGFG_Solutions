625682ffe3f70df80ec1069c819b25f64003c45f
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-09-14 23:17:44 +0530

    Time: 101 ms (71.49%), Space: 84.9 MB (34.21%) - LeetHub

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxValue = 0;
+        int result = 0;
+        int maxLength = 0;
+
+        for (auto num : nums) {
+            if (num > maxValue) {
+                maxValue = num;
+                result = 0;  // Reset result when new max value is found
+                maxLength = 0;  // Reset the current maxLength
+            }
+
+            if (maxValue == num) {
+                maxLength++;
+            } else {
+                maxLength = 0;
+            }
+
+            result = max(result, maxLength);  // Update result with the max length found so far
+        }
+
+        return result;  // Return the result, not maxLength
+    }
+};
