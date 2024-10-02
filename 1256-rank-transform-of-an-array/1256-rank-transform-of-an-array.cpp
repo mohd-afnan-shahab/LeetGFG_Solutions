62af0eff017964d911dbe126524de6bc51ba87cf
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-10-02 13:15:21 +0530

    Time: 41 ms (95.32%), Space: 39.1 MB (85.62%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     // TIME COMPLEXITY - O(nLogn) and SPACE COMPLEXITY - O(n)
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> tempArr = arr;
-        sort(tempArr.begin(), tempArr.end());
 
-        unordered_map<int, int> mpp;
-        int rank = 1;
-
-        for(int i = 0; i < tempArr.size(); i++){
-            if(mpp.find(tempArr[i]) == mpp.end()){
-                mpp[tempArr[i]] = rank;
-                rank++;
-            }            
-        }
+        if(arr.empty()) return {};
+        
+        vector<pair<int, int>> valueIndex;
 
         for(int i = 0; i < arr.size(); i++){
-            arr[i] = mpp[arr[i]];
+            valueIndex.push_back({arr[i], i});
         }
 
-        return arr;
+        sort(valueIndex.begin(), valueIndex.end());
+
+        vector<int> result(arr.size());
+        int rank = 1;
+
+        result[valueIndex[0].second] = rank;
+
+        for(int i = 1; i < valueIndex.size(); i++){
+            // if current element is not equal to prev element
+            if(valueIndex[i].first != valueIndex[i-1].first){
+                rank++;
+            }
+
+            result[valueIndex[i].second] = rank;
+        }
+
+        return result;
     }
 };
