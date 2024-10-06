9bb46ffa00c94526f13d586d417a36e2f0390468
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-10-06 12:02:12 +0530

    Time: 2 ms (61.11%), Space: 10.1 MB (38.75%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    void permutation(vector<int>& nums, vector<int>& temp,
-                     vector<vector<int>>& ans,
-                     vector<bool>& visited)
-    {
-        if(visited.size() == temp.size()){
+
+    void solve(vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans, vector<bool>& visited){
+
+        // base case : if temp.size() becomes equal to arr.size()
+        if(nums.size() == temp.size()){
             ans.push_back(temp);
             return;
         }
-        for(int i = 0; i < nums.size(); i++){
-            if(visited[i] == 0){
+
+        for(int i = 0; i < visited.size(); i++){
+            if(!visited[i]){
                 visited[i] = 1;
                 temp.push_back(nums[i]);
-                permutation(nums, temp, ans, visited);
 
+                // recursive call
+                solve(nums, temp, ans, visited);
+
+                // backtrack
                 visited[i] = 0;
                 temp.pop_back();
-            }    
+            }
         }
     }
-    
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> temp;
         vector<vector<int>> ans;
-        vector<bool> visited(nums.size(), 0); 
+        vector<bool> visited(nums.size(), 0);
 
-        permutation(nums, temp, ans, visited);
+        solve(nums, temp, ans, visited);
 
         return ans;
     }
