9c50d9f4b345e8ef620fbd5b2020158467d366df
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-10-14 10:52:51 +0530

    Time: 195 ms (57.86%), Space: 75.9 MB (88.03%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    // TIME COMPLEXITY - O(N + K log N) and SPACE COMPLEXITY - O(N)
     long long maxKelements(vector<int>& nums, int k) {
         long long score = 0;
 
-        priority_queue<int> pq(nums.begin(), nums.end());
+        priority_queue<int> pq(nums.begin(), nums.end());       // O(N)
 
         for(int i = 0; i < k; i++){
             int maxElement = pq.top();
-            pq.pop();   // log N
+            pq.pop();   // O(log N)
 
             score = score + maxElement;
             maxElement = ceil(maxElement / 3.0);
 
-            pq.push(maxElement);
+            pq.push(maxElement);    // O(log N)
         }
 
         return score;
