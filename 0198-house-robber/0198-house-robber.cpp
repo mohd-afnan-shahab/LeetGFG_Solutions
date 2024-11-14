38e4f155bb27b7efc232f598fe889f277c9bf872
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-14 17:28:29 +0530

    Time: 0 ms (100%), Space: 9.6 MB (12.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,67 @@
 class Solution {
 public:
     
-    // *********** memoization ***********
+    // // *********** memoization ***********
 
-    int helper(int idx, vector<int>& nums, vector<int>& dp){
+    // int helper(int idx, vector<int>& nums, vector<int>& dp){
 
-        // base case
-        if(idx == 0) return nums[0];
+    //     // base case
+    //     if(idx == 0) return nums[0];
 
-        // step.2
-        if(dp[idx] != -1) return dp[idx];
-        
-        int pick = nums[idx];
-        if(idx > 1) pick += helper(idx - 2, nums, dp);
+    //     // step.2
+    //     if(dp[idx] != -1) return dp[idx];
 
-        int notPick = 0 + helper(idx - 1, nums, dp);
+    //     int pick = nums[idx];
+    //     if(idx > 1) pick += helper(idx - 2, nums, dp);
 
-        // step.1
-        dp[idx] = max(pick, notPick);
+    //     int notPick = 0 + helper(idx - 1, nums, dp);
 
-        return dp[idx];
-    }
+    //     // step.1
+    //     dp[idx] = max(pick, notPick);
+
+    //     return dp[idx];
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+
+    //     int idx = n - 1;
+
+    //     // Initialize 
+    //     vector<int> dp(idx + 1, -1);
+
+    //     return helper(idx, nums, dp);
+    // }
+
+
+
+
+
+
+
+        // *********** Tabulation ***********
+
 
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        int idx = n - 1;
-
         // Initialize 
-        vector<int> dp(idx + 1, -1);
+        vector<int> dp(n); 
 
-        return helper(idx, nums, dp);
+        // base case
+        dp[0] = nums[0];
+
+        for(int i = 1; i < n; i++){
+            
+            int pick = nums[i];
+
+            if(i > 1) pick += dp[i-2];
+
+            int notPick = 0 + dp[i-1];
+
+            dp[i] = max(pick, notPick); 
+        }
+
+        return dp[n - 1];
     }
 };
