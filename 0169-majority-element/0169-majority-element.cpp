847f89de49097d23bf2032eef88858fe0293bfc1
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-17 23:50:12 +0530

    Time: 0 ms (100%), Space: 27 MB (79.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int n = nums.size();
+    // int majorityElement(vector<int>& nums) {
+    //     int n = nums.size();
 
-        map<int, int> mpp;
+    //     map<int, int> mpp;
 
-        for(int i = 0; i < n; i++){
-            mpp[nums[i]]++;
-        }
+    //     for(int i = 0; i < n; i++){
+    //         mpp[nums[i]]++;
+    //     }
 
-        // iterate in map
-        for(auto& it : mpp){
-            if(it.second > (n / 2)){
-                return it.first;
+    //     // iterate in map
+    //     for(auto& it : mpp){
+    //         if(it.second > (n / 2)){
+    //             return it.first;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+
+
+    // moore's voting algo
+
+    int majorityElement(vector<int>& nums){
+
+        int element = 0, count = 0;
+
+        for(int num : nums){
+
+            if(count == 0){
+                element = num;
+                count = 1;
+            }
+
+            else if(num == element){
+                count++;
+            }
+
+            else{
+                count--;
             }
         }
 
-        return -1;
+        // if there exist a majority element
+        return element;
     }
 };
