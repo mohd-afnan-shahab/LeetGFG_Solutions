4e68c118c33548b347c5ebbed8fcdd796633f791
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-23 23:56:26 +0530

    Time: 0 ms (100%), Space: 21.9 MB (27.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,13 +3,13 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int i = 0, j = 0;
-        while(i < n){
-            if(nums[i] != 0){
-                swap(nums[i], nums[j]);
-                j++;
-            } 
-            i++;
+        int i = 0;
+        for(int j = 0; j < n; j++){
+
+            if(nums[j] != 0){
+                swap(nums[j], nums[i]);
+                i++;
+            }
         }
         
     }
