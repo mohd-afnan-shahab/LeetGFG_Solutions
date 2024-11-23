ae385526e58d42743a45a9f77b7588d616b04520
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-23 10:52:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] == '[' || s[i] == '{' || s[i] == '(') st.push(s[i]);
+        int n = s.length();
 
-            else if(st.empty())
-                return false;
-            
-            else {
-                char c = st.top();
+        stack<char> st;
+
+        for(int i = 0; i < n; i++){
+
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+
+                char ch = st.top();
                 st.pop();
-                if(c == '(' && s[i] != ')' || c == '{' && s[i] != '}' || c == '[' && s[i] != ']'){
+
+                if((s[i] == ']' && ch != '[') || 
+                    (s[i] == '}' && ch != '{') || 
+                    (s[i] == ')' && ch != '(')){
                     return false;
                 }
             }
         }
 
-        if(st.empty()) return true;
-        else
-            return false;
+        return st.empty();
     }
 };
