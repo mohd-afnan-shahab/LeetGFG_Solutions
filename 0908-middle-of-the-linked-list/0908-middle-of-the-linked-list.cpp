ce0488490aff361b7455d941ade2d97087ce6351
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-23 20:30:07 +0530

    Time: 0 ms (100%), Space: 9.4 MB (21.6%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -1,37 +1,15 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-// TORTOISE AND HARE APPROACH (slow and fast pointer)
 
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        // Edge case: if the list is empty
-        if (head == nullptr) {
-            return nullptr;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
 
-        // Initialize slowPointer and fastPointer to the head
-        ListNode* slowPointer = head;
-        ListNode* fastPointer = head;
-
-        // Move slowPointer by 1 and fastPointer by 2
-        // Loop will stop when fastPointer reaches the end of the list
-        while (fastPointer != nullptr && fastPointer->next != nullptr) {
-            slowPointer = slowPointer->next;            // slow moves by 1
-            fastPointer = fastPointer->next->next;      // fast moves by 2
-        }
-
-        // slowPointer will now be pointing to the middle node
-        return slowPointer;
+        return slow;
     }
 };
