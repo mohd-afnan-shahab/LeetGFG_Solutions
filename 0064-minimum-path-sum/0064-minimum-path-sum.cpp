be8f71c0dc80a3cbdb8329bebd1f7290fcf0707e
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-24 10:10:47 +0530

    Time: 4 ms (17.57%), Space: 15 MB (28.82%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-
-    int helper(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
-
-        // base case
-        if(i == 0 && j == 0) return grid[0][0];
-
-        if(i < 0 || j < 0) return 1e9;      // cuz we have to find min cost
-
-        // step 1 check before recursive call
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int up = grid[i][j] + helper(i-1, j, grid, dp);
-        int left = grid[i][j] + helper(i, j-1, grid, dp);
-
-        // step 2 store it.
-        dp[i][j] = min(up, left);
-        return dp[i][j];
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();    // row size
         int n = grid[0].size();     // col size
 
-        // initialization
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n));
 
-        return helper(m-1, n-1, grid, dp);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+
+                else{
+                    int up = 1e9;
+                    if(i > 0) up = grid[i][j] + dp[i-1][j];
+
+                    int left = 1e9;
+                    if(j > 0) left = grid[i][j] + dp[i][j-1];
+
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
