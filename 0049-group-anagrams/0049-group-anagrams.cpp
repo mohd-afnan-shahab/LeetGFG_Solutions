b18772e8a680adb2add401b525de321a2d392257
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-25 23:39:28 +0530

    Time: 73 ms (10.82%), Space: 33.2 MB (10.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
         unordered_map<string, vector<string>> mpp;
 
         for(auto& s : strs){
-            string key = s;
 
-            sort(key.begin(), key.end());
-            mpp[key].push_back(s);      // {aet} -> {eat, tea, ate}
+            vector<int> hash(26, 0);
+            for(char c : s){
+                hash[c - 'a']++;
+            }
+
+            // convert freq to string (key)
+            string key = "";
+            for(int count : hash){
+                key += to_string(count) + ",";
+            }
+
+            // group anagram by key
+            mpp[key].push_back(s);
         }
 
         vector<vector<string>> result;
-        
-        for(auto& pair : mpp){
-            result.push_back(pair.second);
+        // iterate in map
+        for(auto& it : mpp){
+            result.push_back(it.second);
         }
 
         return result;
