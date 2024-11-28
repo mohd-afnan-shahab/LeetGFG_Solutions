d60762f8bf40a3a479e4cbd3cb151d9f69441606
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-11-28 22:43:51 +0530

    Time: 4 ms (68.3%), Space: 11.7 MB (5.54%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> mp;
-        for(auto& mag : magazine) mp[mag]++;
+    // bool canConstruct(string ransomNote, string magazine) {
+    //     unordered_map<char, int> mp;
+    //     for(auto& mag : magazine) mp[mag]++;
 
-        for(auto& ch : ransomNote){
-            if(mp[ch] <= 0){
+    //     for(auto& ch : ransomNote){
+    //         if(mp[ch] <= 0){
+    //             return false;
+    //         }
+    //         else{
+    //             mp[ch]--;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+
+
+
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+
+        for(char c : magazine){
+            freq[c - 'a']++;
+        }
+
+        for(char c : ransomNote){
+            if(freq[c - 'a'] <= 0){
                 return false;
             }
-            else{
-                mp[ch]--;
-            }
+            freq[c - 'a']--;
         }
 
         return true;
