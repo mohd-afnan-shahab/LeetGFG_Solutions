d55101bfda3aff1c15bb251bf858a784bcd4abe3
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-12-12 20:33:37 +0530

    Time: 2 ms (61.64%), Space: 13.5 MB (7.74%) - LeetHub

---
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        //Heapify - O(n)
+        priority_queue<int> pq(begin(gifts), end(gifts)); //max-heap
+
+        long long sum = 0;
+        for(int &gift : gifts) { //O(n)
+            sum += gift;
+        }
+
+        while(k--) { //O(k)
+            int maxEl = pq.top(); //O(1)
+            pq.pop(); //O(log(n))
+
+            int remaining = sqrt(maxEl); //Ignoring this time complexity
+            sum -= (maxEl - remaining);
+            pq.push(remaining);//O(log(n))
+        }
+
+        return sum;
+
+    }
+};
