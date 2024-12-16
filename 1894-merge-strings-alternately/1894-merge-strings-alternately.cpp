10199cdcfddc9b09bb47145563a3013cf6ec5c65
Mohd Afnan Shahab <108993317+mohd-afnan-shahab@users.noreply.github.com>
2024-12-16 22:35:59 +0530

    Time: 2 ms (45.75%), Space: 8.8 MB (9.05%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int totalLen = n + m;
+        string ans = "";
+        ans.reserve(totalLen);
+
+        int i = 0;
+        while(i < n || i < m){
+
+            if(i < n) ans += word1[i];
+            if(i < m) ans += word2[i];
+
+            i++;
+        }
+
+        return ans;
+    }
+};
